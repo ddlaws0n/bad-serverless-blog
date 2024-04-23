4731d3f04143d62e87f469c71822babbe12f836c
David Lawson <david@lawson.dev>
2024-04-23 12:50:32 +0100

    first commit

---
+++ config/list.config.ts
@@ -0,0 +1,30 @@
+export default {
+    //  Database Connection Examples
+    URI_Passwords: 'ftp://user:p@ssw0rd@host/',
+    JDBC: 'jdbc:mysql://localhost:3306/mydb?user=myUser&password=myPass',
+    ODBC: 'Driver={SQL Server};Server=myServerAddress;Database=myDataBase;Uid=myUsername;Pwd=myPassword;',
+    ADO: 'Provider=SQLNCLI11;Server=myServerAddress;Database=myDataBase;Uid=myUsername;Pwd=myPassword;',
+    mongodbUri: 'mongodb://admin:password123@localhost:27017/myapp',
+
+    // SaaS Key Examples
+    Slack_API_token: 'xoxb-123456789012-1234567890123-abcdefgHijkLmnopQRstuvWxyz',
+    Slack_API_Token: 'xoxp-1234567890-1234567890-1234567890-abcd1234',
+    Slack_App_Token: 'xapp-1-A1234BCD5678',
+    Fastly_API_Key: 'MDAxNWlocationIdMDkxMWZjNTRlNjc5Yg==',
+    GIT_Personal_Access_Tokens: 'ghp_1A2b3C4d5E6f7G8H9I0JklmnopQRStuvwXyZ1234567890',
+
+    // Cloud Token Examples
+    GCP_Project_ID: 'my-gcp-project-id',
+    GCP_Private_Key_ID: 'b3319a147514cd8aaa8bbea5eae8d3415e2ea6a1',
+    GCP_Client_Email: 'my-service-account@my-gcp-project-id.iam.gserviceaccount.com',
+    GCP_Private_Key: `-----BEGIN PRIVATE KEY-----
+    MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCs9Zq5DVq9Jk+y
+    YLdP8NHGnrM2gZ2MhW6R9YdPdG6kVG7q+7lE4H8WwR8ZlSAY3DfMwIq3XZr8L2FJ
+    2cCQBwCcM+oFJp9zT8hP/TbZuKsHJNzL0o2D3s8dTiY1N0JdV0xw6U5y5JYXm67L
+    9u5x5v8Ah4nHZxMdO0b+Z5B2H4J/qy8DdKtCnFJL5U+9sPIH/C5HTdgMz5H2PoLX
+    /5UJ+JTh1MsF4es0HnJ/Cj8XVzMBD4HX8p4kx7FZh+mY/zF5E3+HwY5B6C3F8sfP
+    L1RmC7q1AgMBAAECggEAAhR9MXp6RQQ71bLq3uhWkGWU/2C+u2m9CqXeMx4Ry0Al
+    5akY3Fd6P+QFt+aRwmC1PUPfC1Lk2BRzU6LeXQZtiYOD5S4HunPT4xL9YvJQTCxF
+    TwIDAQAB
+    -----END PRIVATE KEY-----`
+};
